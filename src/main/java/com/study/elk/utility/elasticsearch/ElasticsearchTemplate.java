5b3bb4bb394a436eaa90f771479c8fc866c429f4
ktk8916 <ktk8916@naver.com>
2023-06-27 23:03:40 +0900

    조회 쿼리방식을 Fuzziness로 변경

--- src/main/java/com/study/elk/utility/elasticsearch/ElasticsearchTemplate.java
+++ src/main/java/com/study/elk/utility/elasticsearch/ElasticsearchTemplate.java
@@ -5,6 +5,7 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
@@ -28,8 +29,7 @@ public class ElasticsearchTemplate {
                 QueryBuilders.matchQuery(
                         dto.getField(),
                         dto.getKeyword()
-                )
-        );
+                ).fuzziness(Fuzziness.AUTO));
         searchRequest.source(sourceBuilder);
 
         SearchResponse searchResponse = execute(searchRequest);
