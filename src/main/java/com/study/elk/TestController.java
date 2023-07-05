0639193e8ecfb70b956c828069ed4ce32433d5cb
ktk8916 <ktk8916@naver.com>
2023-07-05 14:44:05 +0900

    init

---
+++ src/main/java/com/study/elk/TestController.java
@@ -0,0 +1,13 @@
+package com.study.elk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
